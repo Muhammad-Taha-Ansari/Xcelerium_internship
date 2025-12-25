60a42e42bc8a2e35206cdeb29da2e01b8ebda16a
Muhammad Taha Ansari <159691554+Muhammad-Taha-Ansari@users.noreply.github.com>
2025-12-24 20:55:54 -0800

    TASK#3

---
+++ WEEK#1/Encoder/Encoder8_3.sv
@@ -0,0 +1,17 @@
+module Encoder8_3(out, in);
+
+input [7:0] in;
+output reg [2:0] out;
+
+always @(*) begin
+if (in & 8'd1)    out = 3'd0;
+else if (in & 8'd2) out = 3'd1;
+else if (in & 8'd4) out = 3'd2;
+else if (in & 8'd8) out = 3'd3;
+else if (in & 8'd16) out = 3'd4;
+else if (in & 8'd32) out = 3'd5;
+else if (in & 8'd64) out = 3'd6;
+else if (in & 8'd128) out = 3'd7;
+else out = 3'd0;
+end
+endmodule
